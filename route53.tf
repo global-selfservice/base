ee1e8ab672732a3feba6b436e6e9a0fc3787783b
Vinod Sai Kumar <vinodsai.kumar@global.com>
2020-12-03 14:48:47 +0000

    feat: added the external dns zone & peering resources

--- route53.tf
+++ route53.tf
@@ -1,11 +1,11 @@
-#module "external_zone" {
-#  source = "github.com/global-devops-terraform/dns-zone?ref=v0.13.4"
-#
-#  name        = "external-${var.environment}"
-#  domain_name = local.env_dns
-#
-#  tags = {
-#    Product     = local.product
-#    Environment = var.environment
-#  }
-#}
+module "external_zone" {
+  source = "github.com/global-devops-terraform/dns-zone?ref=v0.13.4"
+
+  name        = "external-${var.environment}"
+  domain_name = local.env_dns
+
+  tags = {
+    Product     = local.product
+    Environment = var.environment
+  }
+}
