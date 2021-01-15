eaea0dfd4fe5dc7e478b2acd405170782d54d7df
Rafat Khan <54808490+RyuCaelum@users.noreply.github.com>
2021-01-15 17:06:40 +0000

    fix: remove name tag (#6)

--- bastion.tf
+++ bastion.tf
@@ -17,10 +17,6 @@ module "bastion" {
     var.bastion_nlb_cidr
   ])
 
-  tags = {
-    Name = "bastion"
-  }
-
   ssh_principals = [
     "selfservice-${var.environment}-bastion",
     "selfservice-${var.environment}"
