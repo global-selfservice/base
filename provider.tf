451c2a5a81dc40a66c03ea2106fb564c145974e1
Stuart Clark <stuart.clark@Jahingo.com>
2021-03-04 09:47:49 +0000

    feat: Prevent token timeout (#40)

--- provider.tf
+++ provider.tf
@@ -6,22 +6,13 @@ data "aws_eks_cluster" "cluster" {
   name = local.name
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = local.name
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
 
-terraform {
-  backend "s3" {
-    bucket         = "global-self-service-dev-terraform-state"
-    key            = "base.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["token", "--cluster-id", local.name]
+    command     = "aws-iam-authenticator"
   }
 }
