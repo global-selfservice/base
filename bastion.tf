972895f7d0486311cc8e23c044df3fb0ba7862c7
James Hunsdon <61974479+JHunsGlobal@users.noreply.github.com>
2021-05-06 14:55:06 +0100

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -1,5 +1,5 @@
 module "bastion" {
-  source = "github.com/global-devops-terraform/bastion?ref=v0.61.2"
+  source = "github.com/global-devops-terraform/bastion?ref=v0.63.0"
 
   name            = local.name
   vpc_id          = module.vpc.vpc_id
